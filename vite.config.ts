4fb3ffd9a5569a088a8f377cb72a0482d14d23c5
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2025-08-26 18:32:45 -0400

    nevermind

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ['localhost', 'https://badlogin.mitchk.hackclub.app']
+		allowedHosts: ['localhost', '.mitchk.hackclub.app']
 	}
 });
